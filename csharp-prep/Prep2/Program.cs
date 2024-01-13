42f7c86573bffb8b917efad13dd65b5f70096cc9
icaropb1105 <ang23010@byui.edu>
2024-01-12 22:10:05 -0300

    first week preparation

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+
+        else 
+        {
+            letterGrade = "F";
+        }
+
+            Console.WriteLine($"Your letter grade is {letterGrade}");
+            if (grade > 70)
+            {
+                Console.WriteLine("Congratulations, you passed!");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
     }
 }
